4d1b67aff28c20e0274dddaa2a2d0e341c69c336
Jason Csesznegi <jason@Jason-no-MacBook-Pro.local>
2017-08-03 14:19:19 +0900

    add tojs container

--- src/containers/VisibleTicketList.js
+++ src/containers/VisibleTicketList.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { toggleTicket } from '../actions';
+import { ToJS } from './ToJS'
 import TicketList from '../components/TicketList';
 
 const getVisibleTickets = (tickets, filter) => {
@@ -7,18 +8,19 @@ const getVisibleTickets = (tickets, filter) => {
     case 'SHOW_ALL':
       return tickets;
     case 'SHOW_COMPLETED':
-      return tickets.filter(t => t.completed);
+      return tickets.filter(t => t.get('completed'));
     case 'SHOW_ACTIVE':
-      return tickets.filter(t => !t.completed);
+      return tickets.filter(t => !t.get('completed'));
     default:
       throw new Error('Unknown filter: ' + filter);
   }
 }
 
 const mapStateToProps = (state) => { 
-return {
-  tickets: getVisibleTickets(state.get('tickets'), state.get('visibilityFilter'))
-}};
+  return {
+    tickets: getVisibleTickets(state.get('tickets'), state.get('visibilityFilter'))
+  };
+}
 
 const mapDispatchToProps = {
   onTicketClick: toggleTicket
@@ -27,6 +29,6 @@ const mapDispatchToProps = {
 const VisibleTicketList = connect(
   mapStateToProps,
   mapDispatchToProps
-)(TicketList);
+)(ToJS(TicketList));
 
 export default VisibleTicketList;
